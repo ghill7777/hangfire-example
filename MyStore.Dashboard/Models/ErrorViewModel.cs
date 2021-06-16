5f816601541c16846690918c4237a7f3e18ae950
ghill <ghill@keefegroup.com>
2021-06-16 17:24:24 -0500

    added dashboard

---
+++ MyStore.Dashboard/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyStore.Dashboard.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
